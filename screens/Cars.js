72d8138539e0ea7f036812a4292db70607292ce6
Diana Silva <diana.silva@correo.unimet.edu.ve>
2024-05-17 14:39:55 -0400

    Rutas

---
+++ screens/Cars.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Cars() {
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text>Cars</Text>
+    </View>
+  )
+}
---
+++ screens/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home"
+import Likes from "./Likes"
+import Cars from "./Cars"
+import MyCarsOnRent from "./MyCarsOnRent"
+import AddCar from "./AddCar"
+
+
+
+
+export{
+    Home,
+    Likes,
+    Cars,
+    MyCarsOnRent,
+    AddCar
+}
+
