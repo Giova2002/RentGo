3019cf4661aeb1535ec8c1719436a58ee1059a16
Giova2002 <cianfaglione.giovanna@correo.unimet.edu.ve>
2024-05-20 20:34:50 -0400

    prueba

--- header/Header.jsx
+++ header/Header.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet,Image } from 'react-native'
+import { View, Text, StyleSheet, Image} from 'react-native'
 import React from 'react'
 import Profile from './Profile'; //header/Profile.jsx
 
