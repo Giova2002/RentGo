f45cc7b180ead30bac84f2f3147431d9ddb45cec
Giova2002 <cianfaglione.giovanna@correo.unimet.edu.ve>
2024-06-19 21:14:43 -0400

    arreglos

--- screens/RentalDetails.jsx
+++ screens/RentalDetails.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, StyleSheet, ActivityIndicator, Alert, TouchableOpacity } from "react-native";
+import { View, Text, Image, StyleSheet, ActivityIndicator, Alert, TouchableOpacity, ScrollView, Dimensions } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const firebaseConfig = {
   apiKey: "AIzaSyDLC-pS5Vo8WDeWHnJXnrIe4608MrVyak4",
@@ -21,39 +24,19 @@ const back = require("../assets/Img/arrow.png");
 export default function RentalDetail() {
   const navigation = useNavigation();
   const route = useRoute();
-  const { rentalId } = route.params; // Obtener el ID de la reserva desde las params
+  const { rentalId } = route.params;
 
   const [loading, setLoading] = useState(true);
   const [rentalData, setRentalData] = useState(null);
 
-//   useEffect(() => {
-//     const fetchRentalData = async () => {
-//       try {
-//         const rentalDoc = await getDoc(doc(db, "reserva", rentalId));
-//         if (rentalDoc.exists()) {
-//           setRentalData(rentalDoc.data());
-//         } else {
-//           Alert.alert("Error", "No se encontró la reserva");
-//         }
-//       } catch (error) {
-//         Alert.alert("Error", "Hubo un problema al cargar los datos de la reserva");
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchRentalData();
-//   }, [rentalId]);
-
-useEffect(() => {
+  useEffect(() => {
     const fetchRentalData = async () => {
       try {
         const rentalDoc = await getDoc(doc(db, "reserva", rentalId));
         if (rentalDoc.exists()) {
           const rentalData = rentalDoc.data();
-          const carId = rentalData.id_auto; // Obtener el ID del auto desde la reserva
-  
-          // Obtener información del auto usando carId
+          const carId = rentalData.id_auto;
+
           const carDoc = await getDoc(doc(db, "auto", carId));
           if (carDoc.exists()) {
             const carData = carDoc.data();
@@ -61,7 +44,6 @@ useEffect(() => {
               ...rentalData,
               cedula: carData.cedula,
               carnet: carData.carnet,
-              // Agrega otros campos del auto que necesites mostrar en RentalDetail
             });
           } else {
             Alert.alert("Error", "No se encontraron los datos del auto");
@@ -75,7 +57,7 @@ useEffect(() => {
         setLoading(false);
       }
     };
-  
+
     fetchRentalData();
   }, [rentalId]);
 
@@ -98,46 +80,49 @@ useEffect(() => {
 
   return (
     <View style={styles.container}>
-        <TouchableOpacity onPress={() => navigation.goBack()} activeOpacity={0.9}>
-                <Image source={back} resizeMode="contain" style={styles.arrow} />
-              </TouchableOpacity>
-      <View style={styles.card}>
-        <Text style={styles.title}>Documentos del Carro</Text>
-        <Text style={styles.subtitle}>Cédula</Text>
-        <Image source={{ uri: rentalData.cedula }} style={styles.idPhoto} />
-        
-        <Text style={styles.subtitle}>Certificado de Circulación</Text>
-        <Image source={{ uri: rentalData.carnet}} style={styles.idPhoto} />
-        <View style={styles.detailContainer}>
-            
-          {/* <Text style={styles.detailText}><Text style={styles.detailLabel}>Nombre:</Text> {rentalData.nombre}</Text>
-          <Text style={styles.detailText}><Text style={styles.detailLabel}>Apellido:</Text> {rentalData.apellido}</Text> */}
-          <Text style={styles.title}>Información</Text>
-          <Text style={styles.detailText}><Text style={styles.detailLabel}>Teléfono:</Text> 0{rentalData.numero_contacto}</Text>
-          <Text style={styles.detailText}><Text style={styles.detailLabel}>Auto:</Text> {rentalData.nombre_auto}</Text>
-          <Text style={styles.detailText}><Text style={styles.detailLabel}>Precio Total:</Text> {rentalData.precio_total} $</Text>
+      
+      <ScrollView contentContainerStyle={styles.scrollContainer}
+      showsVerticalScrollIndicator={false}>
+      <TouchableOpacity onPress={() => navigation.goBack()} activeOpacity={0.9}>
+        <Image source={back} resizeMode="contain" style={styles.arrow} />
+      </TouchableOpacity>
+        <View style={styles.card}>
+          <Text style={styles.title}>Documentos del Carro</Text>
+          <Text style={styles.subtitle}>Cédula</Text>
+          <Image source={{ uri: rentalData.cedula }} style={styles.idPhoto} />
+          <Text style={styles.subtitle}>Certificado de Circulación</Text>
+          <Image source={{ uri: rentalData.carnet }} style={styles.idPhoto} />
+          <View style={styles.detailContainer}>
+            <Text style={styles.title}>Información</Text>
+            <Text style={styles.detailText}><Text style={styles.detailLabel}>Teléfono:</Text> 0{rentalData.numero_contacto}</Text>
+            <Text style={styles.detailText}><Text style={styles.detailLabel}>Auto:</Text> {rentalData.nombre_auto}</Text>
+            <Text style={styles.detailText}><Text style={styles.detailLabel}>Precio Total:</Text> {rentalData.precio_total} $</Text>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </View>
-   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#F5F5F5",
     padding: 20,
   },
+
   loadingContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   card: {
-    backgroundColor: "#F5F5F5" ,
+    backgroundColor: "#F5F5F5",
     borderRadius: 20,
     padding: 20,
     width: "90%",
@@ -153,7 +138,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginBottom: 20,
     fontFamily: "Raleway_700Bold",
-    
   },
   idPhoto: {
     width: "100%",
@@ -174,25 +158,23 @@ const styles = StyleSheet.create({
     fontFamily: "Raleway_700Bold",
   },
   arrow: {
-
-    right: 160, 
+    right: 145, 
     marginBottom: 20, 
-    
+    marginTop: 60,
+    position: 'fixed',  
+    zIndex: 1,
   },
-
-  subtitle:{
+  subtitle: {
     fontSize: 15,
     fontWeight: "bold",
     textAlign: 'left',
     marginBottom: 20,
     fontFamily: "Raleway_400Regular",
-    
-
   },
   cargando: {
     alignSelf: "center",
     color: 'black',
-    fontSize: '15',
+    fontSize: 15,
     fontFamily: 'Raleway_700Bold',
   },
 });
